cb34deaf9b64620b5eb1dc1ac789fa6599036745
GALAXY-AI1 <kediriasri036@gmail.com>
2025-10-07 12:02:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+cosnt tompolapa=document.getElementById('sapaButton')
+tombolSapa.addEventListener('click,funtion(){
+                            alet('woii!jenengku adam!);
+                                 });
